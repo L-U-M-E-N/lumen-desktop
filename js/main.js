1ef079f3d5ebfea9f021d871240b83f8c1902bf1
Elanis <elanis@hotmail.com>
2020-12-05 20:25:21 +0100

    Exit button

--- js/main.js
+++ js/main.js
@@ -1,25 +1,6 @@
 const remote = require('electron').remote;
 
 window.addEventListener('load', () => {
-	/*const closebtn = document.querySelector('#window-close-button');
-
-	if(!closebtn) {
-		return;
-	}
-
-	closebtn.addEventListener('click', () => {
-		if(typeof currentWindow === 'undefined') {
-			console.log('Unable to find current window - abort');
-			return;
-		}
-
-		if(currentWindow !== 'index') {
-			remote.getCurrentWindow().close();
-		} else {
-			remote.getGlobal('exitPopup')();
-		}
-	});*/
-
 	document.addEventListener('keydown', (e) => {
 		if (e.which === 116) { // F5
 			window.location.reload();
@@ -43,6 +24,11 @@ window.addEventListener('load', () => {
 		let pageLink = '';
 		if(img.id === 'home') {
 			pageLink = '/tmp/index.html';
+		} else if(img.id === 'exit') {
+			img.addEventListener('click', () => {
+				remote.getCurrentWindow().close();
+			});
+			return;
 		} else {
 			pageLink = '/tmp/' + img.id + '_' + img.id + '.html';
 		}

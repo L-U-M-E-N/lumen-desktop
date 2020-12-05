7773dd992e051e12343b281b5eeac234af2d845f
Elanis <elanis@hotmail.com>
2020-12-05 18:08:44 +0100

    Menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const remote = require('electron').remote;
 
 window.addEventListener('load', () => {
-	const closebtn = document.querySelector('#window-close-button');
+	/*const closebtn = document.querySelector('#window-close-button');
 
 	if(!closebtn) {
 		return;
@@ -18,7 +18,7 @@ window.addEventListener('load', () => {
 		} else {
 			remote.getGlobal('exitPopup')();
 		}
-	});
+	});*/
 
 	document.addEventListener('keydown', (e) => {
 		if (e.which === 116) { // F5
@@ -36,4 +36,19 @@ const viewNames = {};
 window.addEventListener('beforeunload', () => {
 	remote.getGlobal('console').log('Closing ' + currentWindow);
 	remote.getGlobal('closeWindow')(viewNames[currentWindow]);
+});
+
+window.addEventListener('load', () => {
+	for(const img of document.querySelectorAll('.sidebar img')) {
+		let pageLink = '';
+		if(img.id === 'home') {
+			pageLink = '/tmp/index.html';
+		} else {
+			pageLink = '/tmp/' + img.id + '_' + img.id + '.html';
+		}
+
+		img.addEventListener('click', () => {
+			window.location.href = require('electron').remote.app.getAppPath() + pageLink;
+		});
+	}
 });
